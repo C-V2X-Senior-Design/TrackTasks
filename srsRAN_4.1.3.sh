0eaf23185da95fdf278de36c7eff8de35a5a3222
Max Ellsworth <max.ellsworth@gmail.com>
2021-10-09 01:13:28 +0800

    Add files via upload

---
+++ srsRAN_4.1.3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+#sudo pkill srs*
+
+sudo ip netns add ue1
+
+sudo srsepc &
+
+sleep 5
+srsenb --rf.device_name=zmq --rf.device_args="fail_on_disconnect=true,tx_port=tcp://*:2000,rx_port=tcp://localhost:2001,id=enb,base_srate=23.04e6" &
+sleep 5
+sudo srsue --rf.device_name=zmq --rf.device_args="tx_port=tcp://*:2001,rx_port=tcp://localhost:2000,id=ue,base_srate=23.04e6" --gw.netns=ue1
+
+
